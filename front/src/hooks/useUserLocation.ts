e2f4c50bd47463a8e0a787c7f5a748ba09c2117c
HyunWoo Jeong <jrwedo@naver.com>
2025-02-12 16:47:20 +0900

    Fix: 유저가 권한을 업데이트하고 돌아왔는데도 반영이 안되는 이슈 수정

--- front/src/hooks/useUserLocation.ts
+++ front/src/hooks/useUserLocation.ts
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react';
 import Geolocation from '@react-native-community/geolocation';
 import {LatLng} from 'react-native-maps';
+import useAppState from "./useAppState.ts";
 
 export default function useUserLocation() {
   const [userLocation, setUserLocation] = useState<LatLng>({
@@ -8,6 +9,7 @@ export default function useUserLocation() {
     longitude: 126.989862,
   });
   const [isUserLocationError, setIsUserLocationError] = useState(false);
+  const {isComeback} = useAppState();
 
   useEffect(() => {
     Geolocation.getCurrentPosition(
@@ -23,7 +25,7 @@ export default function useUserLocation() {
         enableHighAccuracy: true,
       },
     );
-  }, []);
+  }, [isComeback]);
 
   return {userLocation, isUserLocationError};
 }
